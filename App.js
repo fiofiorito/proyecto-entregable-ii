c8d4967a5e889c6a595529506f3fd7dc3da2a4d5
fiofiorito <124637724+fiofiorito@users.noreply.github.com>
2024-02-02 19:57:03 -0300

    finish basic functionality and styles

--- App.js
+++ App.js
@@ -1,39 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import uuid from 'react-native-uuid';
 
 export default function App() {
+  const [taskList, setTaskList] = useState([]);
+  const [newTask, setNewTask] = useState({
+    title: '',
+    desc: '',
+    id: ''
+  });
+
+  const handleTaskTitle = (title) => {
+    const id = uuid.v4();
+    setNewTask({ ...newTask, title: title, id });
+  }
+  const handleTaskDesc = (desc) => {
+    setNewTask({ ...newTask, desc: desc });
+  }
+  const addTask = () => {
+    setTaskList([...taskList, newTask]);
+    setNewTask({ title: '', desc: '', id: '' });
+  }
+  const handleDelete = () => {
+    console.log("funciona")
+  }
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.title}>Bienvenido!</Text>
       </View>
       <View style={styles.newTaskContainer}>
-        <TextInput style={styles.input} placeholder='New Task' />
-        <Button color='#000' title='+' />
+        <View style={styles.taskInfo}>
+          <TextInput onChangeText={handleTaskTitle} style={styles.input} placeholder='New Task' value={newTask.title} />
+          <TextInput onChangeText={handleTaskDesc} style={styles.input} placeholder='Description' value={newTask.desc} />
+        </View>
+        <Button color='#000' title='+' onPress={addTask} />
       </View>
       <ScrollView style={styles.scrollView}>
-        <View style={styles.task}>
-          <View>
-            <Text>Tarea</Text>
-            <Text>Descripcion Tarea</Text>
-          </View>
-          <Button color='#000' title='delete' />
-        </View>
-        <View style={styles.task}>
-          <View>
-            <Text>Tarea</Text>
-            <Text>Descripcion Tarea</Text>
-          </View>
-          <Button color='#000' title='delete' />
-        </View>
-        <View style={styles.task}>
-          <View>
-            <Text>Tarea</Text>
-            <Text>Descripcion Tarea</Text>
-          </View>
-          <Button color='#000' title='delete' />
-        </View>
-
+        {
+          taskList.map(task => {
+            return <View key={task.id} style={styles.task}>
+              <View>
+                <Text style={styles.taskTitle}>{task.title}</Text>
+                <Text>{task.desc}</Text>
+              </View>
+              <Button color='#000' title='delete' onPress={handleDelete} />
+            </View>
+          })
+        }
       </ScrollView>
     </View>
   );
@@ -52,17 +67,22 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
   input: {
-    width: '90%',
+    width: '95%',
     padding: 10,
     borderWidth: 1,
     borderStyle: 'solid',
     borderColor: '#000',
+  },
+  taskInfo: {
+    width: '92%',
+    gap: 10
   },
   newTaskContainer: {
     width: '90%',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     borderWidth: 1,
     borderStyle: 'solid',
     borderColor: '#000',
@@ -84,5 +104,9 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     paddingVertical: 10,
     paddingHorizontal: 10
+  },
+  taskTitle: {
+    fontWeight: '600',
+    fontSize: 18
   }
 });
